ac31df2cbb8bcd68e45f04a6517719097dee6264
Guillaume Queste <guillaume.queste@laposte.net>
2019-07-20 11:31:48 +0200

    accueil summer

--- src/components/Summer/Summer.js
+++ src/components/Summer/Summer.js
@@ -10,50 +10,109 @@ class Summer extends Component {
             <div>
                 <Header />
                 <div className="bodySummer">
-                    <Link to="/Is">I</Link>
-                    <Link to="/IIs">II</Link>
-                    <Link to="/IIIs">III</Link>
-                    <Link to="/IVs">IV</Link>
-                    <Link to="/Vs">V</Link>
-                    <Link to="/VIs">VI</Link>
-                    <Link to="/VIIs">VII</Link>
-                    <Link to="/VIIIs">VIII</Link>
-                    <Link to="/IXs">IX</Link>
-                    <Link to="/Xs">X</Link>
-                    <Link to="/XIs">XI</Link>
-                    <Link to="/XIIs">XII</Link>
-                    <Link to="/XIIIs">XIII</Link>
-                    <Link to="/XIVs">XIV</Link>
-                    <Link to="/XVs">XV</Link>
-                    <Link to="/XVIs">XVI</Link>
-                    <Link to="/XVIIs">XVII</Link>
-                    <Link to="/XVIIIs">XVIII</Link>
-                    <Link to="/XIXs">XIX</Link>
-                    <Link to="/XXs">XX</Link>
-                    <Link to="/XXIs">XXI</Link>
-                    <Link to="/XXIIs">XXII</Link>
-                    <Link to="/XXIIIs">XXIII</Link>
-                    <Link to="/XXIVs">XXIV</Link>
-                    <Link to="/XXVs">XXV</Link>
-                    <Link to="/XXVIs">XXVI</Link>
-                    <Link to="/XVIIs">XVII</Link>
-                    <Link to="/XVIIIs">XVIII</Link>
-                    <Link to="/XIXs">XIX</Link>
-                    <Link to="/XXs">XX</Link>
-                    <Link to="/XXIs">XXI</Link>
-                    <Link to="/XXIIs">XXII</Link>
-                    <Link to="/XXIIIs">XXIII</Link>
-                    <Link to="/XXIVs">XXIV</Link>
-                    <Link to="/XXVs">XXV</Link>
-                    <Link to="/XXVIs">XXVI</Link>
-                    <Link to="/XXVIIs">XXVII</Link>
-                    <Link to="/XXVIIIs">XXVIII</Link>
-                    <Link to="/XXIXs">XXIX</Link>
-                    <Link to="/XXXs">XXX</Link>
-                    <Link to="/XXXIs">XXXI</Link>
-                    <Link to="/XXXIIs">XXXII</Link>
-                    <Link to="/XXXIIIs">XXXIII</Link>
-                    <Link to="/XXXIVs">XXXIV</Link>
+                    <img src={require("../../img/summer/piste.jpg")} alt="piste" className="piste"></img>
+                    <Link to="/Is">
+                        <img src={require("../../img/summer/Ie.jpg")} alt="Ie" className="Ie affiche"></img>
+                    </Link>
+                    <Link to="/IIs">
+                        <img src={require("../../img/summer/IIe.jpg")} alt="Ie" className="IIe affiche"></img>
+                    </Link>
+                    <Link to="/IIIs">
+                        <img src={require("../../img/summer/IIIe.jpg")} alt="IIIe" className="IIIe affiche"></img>
+                    </Link>
+                    <Link to="/IVs">
+                        <img src={require("../../img/summer/IVe.jpg")} alt="IVe" className="IVe affiche"></img>
+                    </Link>
+                    <Link to="/Vs">
+                        <img src={require("../../img/summer/Ve.jpg")} alt="Ve" className="Ve affiche"></img>
+                    </Link>
+                    <Link to="/VIs">
+                        <img src={require("../../img/summer/VIe.png")} alt="VIe" className="VIe affiche"></img>
+                    </Link>
+                    <Link to="/VIIs">
+                        <img src={require("../../img/summer/VIIe.jpg")} alt="VIIe" className="VIIe affiche"></img>
+                    </Link>
+                    <Link to="/VIIIs">
+                        <img src={require("../../img/summer/VIIIe.png")} alt="VIIIe" className="VIIIe affiche"></img>
+                    </Link>
+                    <Link to="/IXs">
+                        <img src={require("../../img/summer/IXe.jpg")} alt="IXe" className="IXe affiche"></img>
+                    </Link>
+                    <Link to="/Xs">
+                        <img src={require("../../img/summer/Xe.png")} alt="Xe" className="Xe affiche"></img>
+                    </Link>
+                    <Link to="/XIs">
+                        <img src={require("../../img/summer/XIe.png")} alt="XIe" className="XIe affiche"></img>
+                    </Link>
+                    <Link to="/XIIs">
+                        <img src={require("../../img/summer/XIIe.jpg")} alt="XIIe" className="XIIe affiche"></img>
+                    </Link>
+                    <Link to="/XIIIs">
+                        <img src={require("../../img/summer/XIIIe.png")} alt="XIIIe" className="XIIIe affiche"></img>
+                    </Link>
+                    <Link to="/XIVs">
+                        <img src={require("../../img/summer/XIVe.png")} alt="XIVe" className="XIVe affiche"></img>
+                    </Link>
+                    <Link to="/XVs">
+                        <img src={require("../../img/summer/XVe.png")} alt="XVe" className="XVe affiche"></img>
+                    </Link>
+                    <Link to="/XVIs">
+                        <img src={require("../../img/summer/XVIe.png")} alt="XVIe" className="XVIe affiche"></img>
+                    </Link>
+                    <Link to="/XVIIs">
+                        <img src={require("../../img/summer/XVIIe.gif")} alt="XVIIe" className="XVIIe affiche"></img>
+                    </Link>
+                    <Link to="/XVIIIs">
+                        <img src={require("../../img/summer/XVIIIe.png")} alt="XVIIIe" className="XVIIIe affiche"></img>
+                    </Link>
+                    <Link to="/XIXs">
+                        <img src={require("../../img/summer/XIXe.png")} alt="XIXe" className="XIXe affiche"></img>
+                    </Link>
+                    <Link to="/XXs">
+                        <img src={require("../../img/summer/XXe.png")} alt="XXe" className="XXe affiche"></img>
+                    </Link>
+                    <Link to="/XXIs">
+                        <img src={require("../../img/summer/XXIe.png")} alt="XXIe" className="XXIe affiche"></img>
+                    </Link>
+                    <Link to="/XXIIs">
+                        <img src={require("../../img/summer/XXIIe.png")} alt="XXIIe" className="XXIIe affiche"></img>
+                    </Link>
+                    <Link to="/XXIIIs">
+                        <img src={require("../../img/summer/XXIIIe.png")} alt="XXIIIe" className="XXIIIe affiche"></img>
+                    </Link>
+                    <Link to="/XXIVs">
+                        <img src={require("../../img/summer/XXIVe.png")} alt="XXIVe" className="XXIVe affiche"></img>
+                    </Link>
+                    <Link to="/XXVs">
+                        <img src={require("../../img/summer/XXVe.png")} alt="XXVe" className="XXVe affiche"></img>
+                    </Link>
+                    <Link to="/XXVIs">
+                        <img src={require("../../img/summer/XXVIe.png")} alt="XXVIe" className="XXVIe affiche"></img>
+                    </Link>
+                    <Link to="/XVIIs">
+                        <img src={require("../../img/summer/XXVIIe.png")} alt="XXVIIe" className="XXVIIe affiche"></img>
+                    </Link>
+                    <Link to="/XVIIIs">
+                        <img src={require("../../img/summer/XXVIIIe.png")} alt="XXVIIIe" className="XXVIIIe affiche"></img>
+                    </Link>
+                    <Link to="/XIXs">
+                        <img src={require("../../img/summer/XXIXe.png")} alt="XXIXe" className="XXIXe affiche"></img>
+                    </Link>
+                    <Link to="/XXs">
+                        <img src={require("../../img/summer/XXXe.png")} alt="XXXe" className="XXXe affiche"></img>
+                    </Link>
+                    <Link to="/XXXIs">
+                        <img src={require("../../img/summer/XXXIe.png")} alt="XXXIe" className="XXXIe affiche"></img>
+                    </Link>
+                    <Link to="/XXXIIs">
+                        <img src={require("../../img/summer/XXXIIe.png")} alt="XXXIIe" className="XXXIIe affiche"></img>
+                    </Link>
+                    <Link to="/XXXIIIs">
+                        <img src={require("../../img/summer/XXXIIIe.png")} alt="XXXIIIe" className="XXXIIIe affiche"></img>
+                    </Link>
+                    <Link to="/XXXIVs">
+                        <img src={require("../../img/summer/XXXIVe.png")} alt="XXXIVe" className="XXXIVe affiche"></img>
+                    </Link>
                 </div>
             </div>
         )
